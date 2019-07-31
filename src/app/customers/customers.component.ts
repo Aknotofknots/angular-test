aa976879f08da1fbb17ba87d3c110819f1999878
Aknotofknots <christianahlsen@gmail.com>
2019-07-31 23:27:24 +0200

    Initial commit

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,3 +1,4 @@
+import { ICustomer } from '../shared/interfaces';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class CustomersComponent implements OnInit {
   title: string;
-  people: object[];
+  people: ICustomer[];
 
   constructor(){}
 
